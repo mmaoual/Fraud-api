9efdac6f181d5fd3370997137d782a94d1bb9cf5
mmaoual <mmaoual@gmail.com>
2022-05-24 08:56:36 +0200

    maj dockerfile

--- setup.sh
+++ setup.sh
@@ -3,14 +3,17 @@ echo "setup and run FRAUD PROJECT docker-compose ..."
 docker volume create --name docker_fraud
 docker network create --subnet 172.55.0.0/16 --gateway 172.55.0.1 mon_reseau
 cd ~/Fraud-api/
-cd performance_image/
+cd API
+docker image build . -t fraud_api:1.0.0
+cd ..
+cd Docker/Performance/
 docker image build . -t performance_image:latest
-cd ../prediction_image/
+cd ../Prediction/
 docker image build . -t prediction_image:latest
-cd ../transactionPrediction_image/
+cd ../TransactionPrediction/
 docker image build . -t transactionPrediction_image:latest
 cd ..
-sudo cp performance_image/performance.py /var/lib/docker/volumes/docker_fraud/_data/prediction.py
-sudo cp prediction_image/prediction.py /var/lib/docker/volumes/docker_fraud/_data/prediction.py
-sudo cp transactionPrediction_image/transactionPrediction.py /var/lib/docker/volumes/docker_fraud/_data/transactionPrediction.py
+sudo cp Performance/performance.py /var/lib/docker/volumes/docker_fraud/_data/prediction.py
+sudo cp Prediction/prediction.py /var/lib/docker/volumes/docker_fraud/_data/prediction.py
+sudo cp TransactionPrediction/transactionPrediction.py /var/lib/docker/volumes/docker_fraud/_data/transactionPrediction.py
 docker-compose up
